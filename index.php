aba4f227e81d91acd32be3301370159e5c080d91
Jenelle Williams <williams1189@mail.philau.edu>
2018-03-27 21:49:22 -0400

    moved to folder

--- index.php
+++ index.php
@@ -1,32 +1,20 @@
 <?php
-
-// Inialize session
-session_start();
-
-// Check, if user is already login, then jump to secured page
-if (isset($_SESSION['username'])) {
-header('Location: securedpage.php');
-}
-
+//include auth.php file on all secure pages
+include("auth.php");
 ?>
+<!DOCTYPE html>
 <html>
-
 <head>
-<title>PinPoint</title>
+<meta charset="utf-8">
+<title>Welcome Home</title>
+<link rel="stylesheet" href="style.css" />
 </head>
-
 <body>
-
- <h3>Welcome to PinPoint</h3>
-
-<table border="0">
-<form method="POST" action="loginproc.php">
-<tr><td>Username</td><td>:</td><td><input type="text" name="username" size="20">        </td></tr>
-<tr><td>Password</td><td>:</td><td><input type="password" name="password"     size="20">    </td></tr>
-<tr><td>&nbsp;</td><td>&nbsp;</td><td><input type="submit" value="Login"></td></tr>
-</form>
-</table>
-
+<div class="form">
+<p>Welcome <?php echo $_SESSION['username']; ?>!</p>
+<p>This is secure area.</p>
+<p><a href="dashboard.php">Dashboard</a></p>
+<a href="logout.php">Logout</a>
+</div>
 </body>
-
 </html>
--- logout.php
+++ logout.php
@@ -1,14 +1,9 @@
 <?php
-
-// Inialize session
 session_start();
-
-// Delete certain session
-unset($_SESSION['username']);
-// Delete all session variables
-// session_destroy();
-
-// Jump to login page
-header('Location: index.php');
-
+// Destroying All Sessions
+if(session_destroy())
+{
+// Redirecting To Home Page
+header("Location: login.php");
+}
 ?>
